3e198710e6af635e1a8a1887f67e6ec85d460136
jaimedevera32-alt <jaimedevera32@gmail.com>
2026-09-09 19:31:53 +0200

    Archivos duplicados y obsoletos a la mierda

--- models/marts/estaciones_clima_procesadas.sql
+++
@@ -1,8 +0,0 @@
-{{ config(materialized='table') }}
-
-SELECT 
-    id_estacion as id,
-    nombre,
-    ST_Transform(ubicacion, 32628) as geometry,
-    altitud
-FROM {{ source('raw_data', 'estaciones_clima') }}
